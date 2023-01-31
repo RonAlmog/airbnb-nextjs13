eaa363a4093ebd9a49956b310681ab193b574298
Ron Almog <ron8132@gmail.com>
2023-01-30 19:35:56 -0500

    fix null on smallcard

--- pages/components/SmallCard.tsx
+++ pages/components/SmallCard.tsx
@@ -8,11 +8,11 @@ function SmallCard({ location }: Props) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointe hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       <div className="relative h-16 w-16 ">
-        <Image src={location.img} fill className="rounded-lg" alt="" />
+        <Image src={location?.img} fill className="rounded-lg" alt="" />
       </div>
       <div>
-        <h2>{location.location}</h2>
-        <h3 className="text-gray-500">{location.distance}</h3>
+        <h2>{location?.location}</h2>
+        <h3 className="text-gray-500">{location?.distance}</h3>
       </div>
     </div>
   );
