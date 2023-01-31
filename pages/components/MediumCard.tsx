996dd9c757c93642f42fa090f23acd378af221c9
Ron Almog <ron8132@gmail.com>
2023-01-30 19:22:51 -0500

    fix images

--- pages/components/MediumCard.tsx
+++ pages/components/MediumCard.tsx
@@ -8,7 +8,7 @@ function MediumCard({ liveAnywherePlace }: Props) {
   return (
     <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
       <div className="relative h-80 w-80">
-        <Image fill src={liveAnywherePlace.img} className="rounded-xl" />
+        <Image fill src={liveAnywherePlace.img} className="rounded-xl" alt="" />
       </div>
       <h3 className="text-2xl mt-3">{liveAnywherePlace.title}</h3>
     </div>
--- pages/components/SmallCard.tsx
+++ pages/components/SmallCard.tsx
@@ -8,7 +8,7 @@ function SmallCard({ location }: Props) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointe hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       <div className="relative h-16 w-16 ">
-        <Image src={location.img} fill className="rounded-lg" />
+        <Image src={location.img} fill className="rounded-lg" alt="" />
       </div>
       <div>
         <h2>{location.location}</h2>
