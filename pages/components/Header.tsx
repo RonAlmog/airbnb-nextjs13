0d43144d56d661491df77a023ebc54f7700a37f2
Ron Almog <ron8132@gmail.com>
2023-01-31 10:17:53 -0500

    add link to home page

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -11,12 +11,14 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker, Range, RangeKeyDict } from "react-date-range";
+import { useRouter } from "next/router";
 
 function Header() {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [numOfGuests, setNumOfGuests] = useState(1);
+  const router = useRouter();
 
   const resetInput = () => {
     setSearchInput("");
@@ -35,7 +37,10 @@ function Header() {
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:p-10">
-      <div className="relative flex items-center h-10 w-20 cursor-pointer">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 w-20 cursor-pointer"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           fill
--- pages/search.tsx
+++ pages/search.tsx
@@ -7,7 +7,7 @@ function Search() {
     <div className="">
       <Header />
       <main className="flex">
-        <section>
+        <section className="flex-grow pt-14 px-6">
           <p className="text-xs">300+ Stays for you</p>
           <h1 className="text-3xl font-semibold nt-2 mb-6">Stays in mars</h1>
           <div className="hidden lg:inline-flex space-x-3 text-gray-800 whitespace-nowrap">
